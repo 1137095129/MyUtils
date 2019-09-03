eafdd12b5d69dd585fa8a785796935b519042a4c
1137095129 <wang1137095129@foxmail.com>
2019-09-03 17:02:26 +0800

    test

--- MyOPT/opttest/src/main/java/com/myopttest/opttest/untils/UserNotFoundException.java
+++ MyOPT/opttest/src/main/java/com/myopttest/opttest/untils/UserNotFoundException.java
@@ -1,9 +1,6 @@
 package com.myopttest.opttest.untils;
 
 public class UserNotFoundException extends Throwable {
-    public UserNotFoundException(){
-        super();
-    }
     public UserNotFoundException(String information){
         super(information);
     }
